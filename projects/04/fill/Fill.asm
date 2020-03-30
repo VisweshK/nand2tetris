fa8277c80039fc88da2b8a9857b52fcb2e1a81da
visweshk <krishnaviswesh@gmail.com>
2020-03-30 10:58:22 +0530

    upd: finish week 4

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -11,4 +11,44 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+@color
+M=0
+
+(LOOP)
+    @SCREEN
+    D=A
+    @pixels
+    M=D
+
+    @KBD
+    D=M
+    @BLACK
+    D;JGT
+
+    @color
+    M=0
+    @COLOR_SCREEN
+    0;JMP
+
+    (BLACK)
+        @color
+        M=-1
+
+    (COLOR_SCREEN)
+        @color
+        D=M
+        @pixels
+        A=M
+        M=D
+        
+        @pixels
+        M=M+1
+        D=M
+
+        @24576
+        D=D-A
+        @COLOR_SCREEN
+        D;JLT
+    
+    @LOOP
+    0;JMP
--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -7,3 +7,26 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+    @i
+    M=1
+    @R2
+    M=0
+(LOOP)
+    @i
+    D=M
+    @R0
+    D=D-M
+    @END
+    D;JGT
+    @R1
+    D=M
+    @R2
+    M=D+M
+    @i
+    M=M+1
+    @LOOP
+    0;JMP
+(END)
+    @END
+    0;JMP
+
